669af3e6ef7b084b6834f7b4cae094c23d8b5d66
Anna Pankratova <annapank24@yandex.ru>
2024-10-15 23:01:29 +0300

    Fix bug: apple can no longer appear on snake's body

--- the_snake.py
+++ the_snake.py
@@ -185,6 +185,8 @@ def main():
         if new_snake_position == apple.position:
             snake.length += 1
             apple.randomize_position()
+            while apple.position in snake.positions:
+                apple.randomize_position()
         if new_snake_position in snake.positions[2:]:
             snake.reset()
             screen.fill(BOARD_BACKGROUND_COLOR)
